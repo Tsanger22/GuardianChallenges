73a0d88e0b722bec9b67b29e23bdc35cdd2e1b6e
Tsanger22 <98707774+Tsanger22@users.noreply.github.com>
2025-07-18 15:55:34 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,20 +5,7 @@ const restrictions = [
   "Swap Characters",
   "Spec all stats to Health",
   "No Aspects",
-  "No Surges",
-  "Fusion Rifles in each slot",
-  "Equip a sidearm",
-  "Swap heavy",
-  "No Fragments",
-  "Can’t use super",
-  "Equip a waveframe",
-  "Double special",
-  "Put on an off-meta exotic",
-  "No 5th level artifact",
-  "Only one element for damage",
-  "No finishers",
-  "No fonts",
-  "Use a sword"
+  "No Surges"
 ];
 
 const rollBtn = document.getElementById("rollbtn");
@@ -26,22 +13,10 @@ const result = document.getElementById("result");
 const selectedChallengesList = document.getElementById("selectedChallengesList");
 
 rollBtn.addEventListener("click", () => {
-  rollBtn.disabled = true;
+  const choice = restrictions[Math.floor(Math.random() * restrictions.length)];
+  result.textContent = choice;
 
-  let count = 0;
-  const maxCount = 20;
-
-  const spin = setInterval(() => {
-    const choice = restrictions[Math.floor(Math.random() * restrictions.length)];
-    result.textContent = choice;
-    count++;
-    if (count > maxCount) {
-      clearInterval(spin);
-      rollBtn.disabled = false;
-
-      const li = document.createElement("li");
-      li.textContent = choice;
-      selectedChallengesList.appendChild(li);
-    }
-  }, 100);
+  const li = document.createElement("li");
+  li.textContent = choice;
+  selectedChallengesList.appendChild(li);
 });
